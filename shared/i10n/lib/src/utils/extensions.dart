2629e0de4867f8885a4fa047cf53c8031a3fc7df
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-02-23 15:20:04 +0800

    refactor(i10n): update i10n extension

--- shared/i10n/lib/src/utils/extensions.dart
+++ shared/i10n/lib/src/utils/extensions.dart
@@ -2,5 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:i10n/i10n.dart';
 
 extension TranslationExtension on BuildContext {
-  Translations get i10n => Translations.of(this);
+  String get thousandSeparator => switch (LocaleSettings.currentLocale) {
+        AppLocale.id => '.',
+        AppLocale.en => ',',
+      };
+  String get decimalSeparator => switch (LocaleSettings.currentLocale) {
+        AppLocale.id => ',',
+        AppLocale.en => '.',
+      };
 }
